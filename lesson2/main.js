cd50177916fad87ce2f232b09409bfb8f629edad
mereeva <mereeva21@gmail.com>
2018-07-30 13:28:30 +0530

    lesson2

---
+++ lesson2/main.js
@@ -0,0 +1,10 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      product: 'Socks',
+      description: "Socks of high quality",
+      image: "./assets/brown-socks.jpeg",
+      altText: "Brown socks",
+      url:"./assets/brown-socks.jpeg"
+    }
+  })
