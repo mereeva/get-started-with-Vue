957e933055a9adcbbc695419aa20f136d0344115
mereeva <mereeva21@gmail.com>
2018-07-30 13:57:32 +0530

    lesson3

---
+++ lesson3/main.js
@@ -0,0 +1,12 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+      product: 'Socks',
+      description: "Socks of high quality",
+      image: "./assets/brown-socks.jpeg",
+      altText: "Brown socks",
+      url:"./assets/brown-socks.jpeg",
+      onSale:true,
+      inventory:11,
+    }
+  })
